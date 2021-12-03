1c4f81724a051d0107ce2b249d6b47ee54653829
Timothy Moores <info@tmoor.es>
2021-12-03 09:28:41 +0000

    improved soln

--- day3.erl
+++ day3.erl
@@ -8,69 +8,42 @@ addA(<<$1:8, Tl/binary>>) -> [{1,0}|addA(Tl)];
 addA(<<$0:8, Tl/binary>>) -> [{0,1}|addA(Tl)];
 addA(<<>>) -> [].
 
-part1([X]) -> addA(X);
-part1([Hd|Tl]) ->
- X = part1(Tl),
+get_cnt([X]) -> addA(X);
+get_cnt([Hd|Tl]) ->
+ X = get_cnt(Tl),
  [{A+B,C+D} || {{A,C},{B,D}} <- lists:zip(X, addA(Hd))].
 
-resolve1([]) -> {<<>>, <<>>};
-resolve1([{_A,_B}|Tl]) when _A > _B ->
-  {X, Y} = resolve1(Tl),
-  {<<1:1, X:(bit_size(X))/bits>>, <<0:1, Y:(bit_size(Y))/bits>>};
-resolve1([_|Tl]) ->
-  {X, Y} = resolve1(Tl),
-  {<<0:1, X:(bit_size(X))/bits>>, <<1:1, Y:(bit_size(Y))/bits>>}.
+invert(<<$1:8,Tl/binary>>) -> <<$0, (invert(Tl))/binary>>;
+invert(<<$0:8,Tl/binary>>) -> <<$1, (invert(Tl))/binary>>;
+invert(<<>>) -> <<>>.
 
-get_most_common([{<<$1:8,_/binary>>, _}|Tl]) ->
-  {A,B} = get_most_common(Tl),
-  {1+A,B};
-get_most_common([{<<$0:8,_/binary>>, _}|Tl]) ->
-  {A,B} = get_most_common(Tl),
-  {A,B+1};
-get_most_common([]) -> {0,0}.
+part1(List) ->
+  L = get_cnt(List),
+  X = list_to_binary([if A>B -> $1; true -> $0 end || {A,B} <- L]),
+  toint(X) * toint(invert(X)).
 
-fp(<<X:8,_/binary>>) -> X.
-lp(<<_:8,X/binary>>) -> X.
+get_most_common([{<<Hd:8,_/binary>>, _}|Tl]) -> Hd - $0 + get_most_common(Tl);
+get_most_common([]) -> 0.
 
-filter_most(X, {_A,_B}) when _A >= _B ->
-  [{lp(A1), A2} || {A1,A2} <- X, fp(A1) =:= $1];
-filter_most(X, {_A,_B}) ->
-  [{lp(A1), A2} || {A1,A2} <- X, fp(A1) =:= $0].
+toint(<<>>) -> 0;
+toint(X) ->
+  <<Hd:(byte_size(X)-1)/binary,Y:8>> = X,
+  (toint(Hd) bsl 1) + (Y band 1).
 
-filter_least(X, {_A,_B}) when _A < _B ->
-  [{lp(A1), A2} || {A1,A2} <- X, fp(A1) =:= $1];
-filter_least(X, {_A,_B}) ->
-  [{lp(A1), A2} || {A1,A2} <- X, fp(A1) =:= $0].
-
-co2([{_,X}]) -> X;
-co2(Mc) ->
-  {M1,M2} = get_most_common(Mc),
-  co2(filter_least(Mc, {M1, M2})).
-
-oxy([{_,X}]) -> X;
-oxy(Mc) ->
-  {M1,M2} = get_most_common(Mc),
-  oxy(filter_most(Mc, {M1, M2})).
-
-toint(<<$1:8,Tl/binary>>) -> <<1:1,(toint(Tl))/bits>>;
-toint(<<$0:8,Tl/binary>>) -> <<0:1,(toint(Tl))/bits>>;
-toint(<<>>) -> <<>>.
+part2([{_,X}], _) -> toint(X);
+part2(Mc, Fun) ->
+  X = Fun(get_most_common(Mc), length(Mc) / 2),
+  part2([{binary:last(A1), A2} || {A1,A2} <- Mc, (binary:first(A1) =:= $1) =:= X], Fun).
 
 main(["1"]) ->
   {ok, Input} = file:read_file(?INPUT),
   List = [X || X <- string:split(Input, "\n", all), X /= <<"">>],
-  X = part1(List),
-  {C,D} = resolve1(X),
-  <<A:(bit_size(C))>> = C,
-  <<B:(bit_size(D))>> = D,
-  io:fwrite("~p~n", [A*B]);
+  io:fwrite("~p~n", [part1(List)]);
 
 main(["2"]) ->
   {ok, Input} = file:read_file(?INPUT),
   List = [{X,X} || X <- string:split(Input, "\n", all), X /= <<"">>],
-  C = toint(oxy(List)),
-  D = toint(co2(List)),
-  <<A:(bit_size(C))>> = C,
-  <<B:(bit_size(D))>> = D,
+  A = part2(List, fun erlang:'>='/2),
+  B = part2(List, fun erlang:'<'/2),
   io:fwrite("~p~n", [A*B]).
 
